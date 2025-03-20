245877fd8bb47ff419aac33b56566ef6820349a3
Sadavarsh <sadavarshsaini1@gmail.com>
2025-03-20 18:32:19 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/db.js";
+import cors from "cors";
+import cookieParse from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import companyRouter from "./routes/company.routes.js";
+import jobRouter from "./routes/job.routes.js";
+import applicationRouter from "./routes/application.routes.js";
+
+
+const app = express();
+dotenv.config()
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParse());
+
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+//routes
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/company", companyRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port at ${PORT}`);
+});
